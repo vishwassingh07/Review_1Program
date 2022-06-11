fcc702d8483b3db5a73940172634a97dd0e5da45
Vishwas <baghel.vishwas0@gmail.com>
2022-06-11 14:44:27 +0530

    [Vishwas]add: Review_1 Checking Even Odd Program Added

---
+++ EvenOddProgram/CheckingEvenOdd.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOddProgram
+{
+    public class CheckingEvenOdd
+    {
+        public static void EvenOdd()
+        {
+            Console.Write("Enter any number to check whether its even or not : ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("The given number {0} is Even ", number);
+            }
+            else
+            {
+                Console.WriteLine("The given number {0} is Odd ", number);
+            }
+        }
+    }
+}
---
+++ EvenOddProgram/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace EvenOddProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Checking Even or Odd program");
+            CheckingEvenOdd.EvenOdd();
+            
+
+        }
+    }
+}
